d04658e5987674495fd90a12e0492a582b0ad491
Dima Malofeev <dim2k2006@yandex.ru>
2015-08-14 02:23:33 +0300

    working project

---
+++ app/js/module.js
@@ -0,0 +1,143 @@
+var player = (function() {   
+    var options = {
+        context: new window.AudioContext(),
+        source: null,
+        audioBuffer: null,
+        wavesurfer: Object.create(WaveSurfer),
+        waveContainer: document.querySelector('#wave'),
+        waveColor: 'white',
+        progressColor: 'white',
+        startButton: $('.player__toggle_start'),
+        stopButton: $('.player__toggle_stop'),
+        metaTitle: null,
+        metaArtist: null,
+        metaAlbum: null,
+        metaYear: null,
+        dv: '',
+        reader: '',
+        filename: null,
+        playerContainer: $('.player'),
+        playerToggler: $('.player__toggle'),
+        autoTrigger: false,
+        timeOut: 0     
+    };
+    
+
+
+    options.wavesurfer.init({
+        container: options.waveContainer,
+        waveColor: options.waveColor,
+        progressColor: options.progressColor
+    });
+
+
+
+    options.wavesurfer.on('ready', function() {
+        options.playerContainer.removeClass('player__loading');
+
+
+        if (options.filename) {
+            $('#filename').html(options.filename);
+        }
+        
+        if (options.metaTitle) {
+            $('#meta-name').html(options.metaTitle);
+        }
+
+        if (options.metaArtist) {
+            $('#meta-artist').html('by <u>' + options.metaArtist + '</u>');
+        }
+        
+        if (options.metaAlbum || options.metaYear) {
+            $('#meta-album').html(options.metaAlbum + ' ' + options.metaYear);
+        }
+        
+        if (!options.metaTitle && !options.metaArtist && !options.metaAlbum && !options.metaYear) {
+            $('#meta-name').html('no metadata');
+        }
+
+        // options.autoTrigger = true;
+        // options.startButton.trigger('click');      
+    });
+        
+
+
+    function _setUpListners() {
+        options.startButton.click(function(event) {
+            event.preventDefault();
+            options.source = options.context.createBufferSource();
+            options.source.buffer = options.audioBuffer;
+            options.source.loop = false;
+            options.source.connect(options.context.destination);
+
+            if (options.autoTrigger) {
+                options.timeOut = 2000;
+            } else {
+                options.timeOut = 0;
+            }
+
+            setTimeout(function() {
+                options.source.start(0);
+                options.startButton.removeClass('player__toggle_active');
+                options.stopButton.addClass('player__toggle_active');
+            }, options.timeOut);
+
+            options.autoTrigger = false;
+        });
+
+
+
+        options.stopButton.click(function(event) {
+            event.preventDefault();
+            if (options.source) {
+                options.source.stop(0);
+                $(this).removeClass('player__toggle_active');
+                options.startButton.addClass('player__toggle_active');
+            }
+        });
+
+
+
+        $('input[type="file"]').change(function(e) {
+            if (options.stopButton.hasClass('player__toggle_active')) {
+                options.stopButton.trigger('click');
+            }
+            options.playerContainer.addClass('player__loading');
+            options.playerToggler.removeClass('player__inactive');
+            options.reader = new FileReader();
+            options.reader.onload = function(e) {
+                _initSound(e.target.result);
+
+                options.dv = new jDataView(this.result);
+                if (options.dv.getString(3, options.dv.byteLength - 128) == 'TAG') {
+                    options.metaTitle = options.dv.getString(30, options.dv.tell());
+                    options.metaArtist = options.dv.getString(30, options.dv.tell());
+                    options.metaAlbum = options.dv.getString(30, options.dv.tell());
+                    options.metaYear = options.dv.getString(4, options.dv.tell());
+                }
+            };
+
+            options.filename = e.target.files[0].name;
+            options.reader.readAsArrayBuffer(e.target.files[0]);
+            options.wavesurfer.loadBlob(e.target.files[0]);
+        });       
+    }
+
+
+
+    function _initSound(arrayBuffer) {
+        options.context.decodeAudioData(arrayBuffer, function(buffer) {
+            options.audioBuffer = buffer;
+        }, function(e) {
+            console.log('Error decoding', e);
+        }); 
+    }
+
+
+
+    return {
+        init: function () {
+            _setUpListners();
+        }
+    };
+}());
--- app/js/script.js
+++ app/js/script.js
@@ -1,89 +1,18 @@
-var wavesurfer = Object.create(WaveSurfer);
-
-wavesurfer.init({
-    container: document.querySelector('#wave'),
-    waveColor: 'white',
-    progressColor: 'white'
+$(document).ready(function(){
+	player.init();
 });
 
-if (!window.AudioContext) {
-  alert('The Web Audio API is not supported in your browser!');
-}
-var context = new window.AudioContext();
-var source = null;
-var audioBuffer = null;
- 
-function stopSound() {
-  if (source) {
-    source.stop(0);
-  }
-}
-
-function playSound() {
-  source = context.createBufferSource(); // Global so we can .noteOff() later.
-  source.buffer = audioBuffer;
-  source.loop = false;
-  source.connect(context.destination);
-  source.start(0);    
-}
-
-function initSound(arrayBuffer) {
-  context.decodeAudioData(arrayBuffer, function(buffer) {
-    audioBuffer = buffer;
-    var buttons = document.querySelectorAll('button');
-    buttons[0].disabled = false;
-    buttons[1].disabled = false;
-  }, function(e) {
-    console.log('Error decoding', e);
-  }); 
-}
-
-document.querySelector('input[type="file"]').addEventListener('change', function(e) {  
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    initSound(e.target.result);
-
     
-    var dv = new jDataView(this.result);
-    if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
-      var title = dv.getString(30, dv.tell());
-      var artist = dv.getString(30, dv.tell());
-      var album = dv.getString(30, dv.tell());
-      var year = dv.getString(4, dv.tell());
 
 
-      document.getElementById('meta-name').innerHTML = title;
-      document.getElementById('meta-artist').innerHTML = 'by <u>' + artist + '</u>';
-      document.getElementById('meta-album').innerHTML = album + ' ' + year;
-      // document.getElementById('meta').innerHTML = 'title: ' + title
-      // + '<br>'
-      // + 'artist: '
-      // + artist
-      // + '<br>'
-      // + 'album: '
-      // + album
-      // + '<br>'
-      // + 'year: '
-      // + year;
-    } else {
-      // no ID3v1 data found.
-      document.getElementById('filename').innerHTML = 'no metadata';
-    }
-  };
-  reader.readAsArrayBuffer(e.target.files[0]);
-  document.getElementById('filename').innerHTML = e.target.files[0].name;
 
-  // wavesurfer.load(e.target.files[0]);
-   wavesurfer.loadBlob(e.target.files[0]);
-}, false);
+
+ 
+
+
+
+
+
+
+
   
-// Example loading via xhr2: loadSoundFile('sounds/A220_A880.wav');
-function loadSoundFile(url) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-  request.onload = function(e) {
-    initSound(e.target.response);
-  };
-  request.send();
-}
